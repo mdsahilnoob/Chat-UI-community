60bc4e82bd14152706c209535a2e771b34bec2b7
Mohammad Sahil <144834170+mdsahilnoob@users.noreply.github.com>
2024-03-02 19:00:09 +0530

    Add files via upload

---
+++ Chat UI kit (Community)/src/pages/ChatUI.js
@@ -0,0 +1,170 @@
+import styles from "./ChatUI.module.css";
+
+const ChatUI = () => {
+  return (
+    <div className={styles.chatUi}>
+      <img className={styles.chatUiChild} alt="" src="/frame-2916.svg" />
+      <div className={styles.chatUiItem} />
+      <img className={styles.chatUiInner} alt="" src="/rectangle-3111.svg" />
+      <div className={styles.rectangleDiv} />
+      <div className={styles.chatUiChild1} />
+      <div className={styles.chatUiChild2} />
+      <img className={styles.rectangleIcon} alt="" src="/rectangle-3115.svg" />
+      <div className={styles.chatUiChild3} />
+      <div className={styles.chatUiChild4} />
+      <div className={styles.chatUiChild5} />
+      <div className={styles.chatUiChild6} />
+      <div className={styles.chatUiChild7} />
+      <div className={styles.chatUiChild8} />
+      <div className={styles.chatUiChild9} />
+      <div className={styles.ellipseDiv} />
+      <div className={styles.chatUiChild10} />
+      <div className={styles.chatUiChild11} />
+      <div className={styles.chatUiChild12} />
+      <div className={styles.chatUiChild13} />
+      <div className={styles.chatUiChild14} />
+      <div className={styles.chatUiChild15} />
+      <div className={styles.chatUiChild16} />
+      <div className={styles.chatUiChild17} />
+      <div className={styles.chatUiChild18} />
+      <div className={styles.chatUiChild19} />
+      <div className={styles.chatUiChild20} />
+      <div className={styles.chatUiChild21} />
+      <div className={styles.chatUiChild22} />
+      <div className={styles.chatUiChild23} />
+      <div className={styles.chatUiChild24} />
+      <div className={styles.chatUiChild25} />
+      <div className={styles.chatUiChild26} />
+      <div className={styles.chatUiChild27} />
+      <div className={styles.chatUiChild28} />
+      <div className={styles.chatUiChild29} />
+      <div className={styles.personPersonemailcom}>
+        <p className={styles.person}>Person</p>
+        <p className={styles.personemailcom}>Person@email.com</p>
+      </div>
+      <div className={styles.person1Person1emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 1</p>
+          <p className={styles.personemailcom}>Person1@email.com</p>
+        </span>
+      </div>
+      <div className={styles.all}>All</div>
+      <div className={styles.inbox}>Inbox</div>
+      <div className={styles.person2Person2emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 2</p>
+          <p className={styles.personemailcom}>Person2@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person3Person3emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 3</p>
+          <p className={styles.personemailcom}>Person3@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person4Person4emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 4</p>
+          <p className={styles.personemailcom}>Person4@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person6Person6emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 6</p>
+          <p className={styles.personemailcom}>Person6@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person7Person7emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 7</p>
+          <p className={styles.personemailcom}>Person7@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person8Person8emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 8</p>
+          <p className={styles.personemailcom}>Person8@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person9Person9emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 9</p>
+          <p className={styles.personemailcom}>Person9@email.com</p>
+        </span>
+      </div>
+      <div className={styles.person5Person5emailcomContainer}>
+        <span className={styles.person1Person1emailcomContainer1}>
+          <p className={styles.person}>Person 5</p>
+          <p className={styles.personemailcom}>Person5@email.com</p>
+        </span>
+      </div>
+      <img
+        className={styles.magnifyingGlassSolid1Icon}
+        alt=""
+        src="/magnifyingglasssolid-1.svg"
+      />
+      <div className={styles.chatUiChild30} />
+      <div className={styles.chatUiChild31} />
+      <div className={styles.chatUiChild32} />
+      <div className={styles.chatUiChild33} />
+      <div className={styles.chatUiChild34} />
+      <div className={styles.chatUiChild35} />
+      <img
+        className={styles.faceSmileRegular1Icon}
+        alt=""
+        src="/facesmileregular-1.svg"
+      />
+      <div className={styles.chatUiChild36} />
+      <div className={styles.chatUiChild37} />
+      <div className={styles.chatUiChild38} />
+      <div className={styles.chatUiChild39} />
+      <div className={styles.you}>You</div>
+      <div className={styles.you1}>You</div>
+      <div className={styles.friday200Pm}>Friday 2:00 pm</div>
+      <div className={styles.friday203Pm}>Friday 2:03 pm</div>
+      <div className={styles.saturday720Pm}>Saturday 7:20 pm</div>
+      <div className={styles.friday202Pm}>Friday 2:02 pm</div>
+      <div className={styles.saturday702Am}>Saturday 7:02 am</div>
+      <div className={styles.person10}>Person</div>
+      <div className={styles.person11}>Person</div>
+      <div className={styles.person12}>Person</div>
+      <div className={styles.chatUiChild40} />
+      <div className={styles.today}>Today</div>
+      <img
+        className={styles.arrowRightSolid1Icon}
+        alt=""
+        src="/arrowrightsolid-1.svg"
+      />
+      <div className={styles.sendYourMessage}>Send your message...</div>
+      <img
+        className={styles.bookmarkRegular1Icon}
+        alt=""
+        src="/bookmarkregular-1.svg"
+      />
+      <img
+        className={styles.folderRegular1Icon}
+        alt=""
+        src="/folderregular-1.svg"
+      />
+      <img className={styles.linkSolid1Icon} alt="" src="/linksolid-1.svg" />
+      <img
+        className={styles.magnifyingGlassSolid2Icon}
+        alt=""
+        src="/magnifyingglasssolid-1.svg"
+      />
+      <div className={styles.conversations}>Conversations</div>
+      <div className={styles.yourTeam}>Your Team</div>
+      <div className={styles.you2}>You</div>
+      <div className={styles.mentions}>Mentions</div>
+      <div className={styles.unassigned}>Unassigned</div>
+      <div className={styles.promotions}>Promotions</div>
+      <div className={styles.support}>Support</div>
+      <div className={styles.person13}>Person 1</div>
+      <div className={styles.person21}>Person 2</div>
+      <div className={styles.person31}>Person 3</div>
+      <div className={styles.inviteYourTeam}>+ Invite your team</div>
+    </div>
+  );
+};
+
+export default ChatUI;
